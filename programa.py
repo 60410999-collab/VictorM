72e87300d6c082e6e0b1ed4e450f2080e49a0752
Victor <60410999@continental.edu.pe>
2025-11-27 13:23:42 -0500

    Primer commit: agregar programa.py

--- programa.py
+++ programa.py
@@ -1,7 +1,9 @@
-a1 = float(input("Ingresa el primer término (a1): "))
+a1 = float(input("Ingresa el primer término (a): "))
 d = float(input("Ingresa la diferencia (d): "))
 n = int(input("Ingresa el número de término que quieres (n): "))
 
 an = a1 + (n - 1) * d
+Sn = n * (a1 + an) / 2
 
-print("El término número", n, "de la progresión aritmética es:", an)
+print("El termino número", n, "es (an):", an)
+print("La suma de los primeros", n, "términos es (Sn):", Sn)
