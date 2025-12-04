4ce77b79779bfe85c2a83f06a8f200181eb7a0a4
60410999-collab <60410999@continental.edu.pe>
2025-12-04 12:09:26 -0500

    Update variacion.py

--- variacion.py
+++ variacion.py
@@ -1,5 +1,8 @@
-def variacion_con_modificacion(n, k):
-    return n ** k
+def factorial(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
 
 n = int(input("Ingresa n (total de elementos): "))
 k = int(input("Ingresa k (lugares a ordenar): "))
@@ -7,5 +10,9 @@ k = int(input("Ingresa k (lugares a ordenar): "))
 if k < 0 or k > n:
     print("k debe estar entre 0 y n.")
 else:
-    V = variacion_con_modificacion(n, k)
+    n_fact = factorial(n)
+    n_k_fact = factorial(n - k)
+
+    V = n_fact / n_k_fact
+
     print("V(", n, ",", k, ") =", V)
